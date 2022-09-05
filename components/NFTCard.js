230ebb082b8318cb8e620979b9e6ff9f02537d50
collo <mutaic84@gmail.com>
2022-09-05 12:29:10 +0300

    style the subinfo component

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -4,6 +4,7 @@ import React from 'react'
 
 import { COLORS, SIZES, SHADOWS, assets } from '../constants'
 import { CircleButton, RectButton } from './Button'
+import { SubInfo, EthPrice, NFTTitle } from './SubInfo'
 const NFTCard = ({ data }) => {
 
   const navigation = useNavigation()
@@ -30,6 +31,8 @@ const NFTCard = ({ data }) => {
 
         <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
+
+      <SubInfo />
     </View>
   )
 }
